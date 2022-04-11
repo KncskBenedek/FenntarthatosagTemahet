0c8453d10246bbf3089a72d2f9dc1345299e124e
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-04-11 23:32:24 +0200

    Főoldal anmíció kész kinézeten kell javítani

---
+++ fooldal.js
@@ -0,0 +1,15 @@
+gsap.registerPlugin(gsap);
+function QS(elem){
+    return document.querySelector(elem)
+}
+
+const hero = QS('.hero')
+const slider = QS('.slider')
+const headline = QS('.headline')
+
+var tl = gsap.timeline();
+
+tl.fromTo(hero,1,{height: "0%"},{ height:"80%", ease: Power2.easeInOut})
+.fromTo(hero, 1.2,{width: "100%"},{ width:"80%", ease: Power2.easeInOut})
+.fromTo(slider,1.2,{x:"-100%"},{x:"0%", ease: Power2.easeInOut},"-=1.2")
+.fromTo(headline,1.2,{x:"-100%"},{x:"10%", ease: Power2.easeInOut},"-=1.2")
