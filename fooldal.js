d900f2b36f3aae1741c476ca5e6975311813cc47
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-04-12 00:32:46 +0200

    nav bár kezdetleges verzió még nincs kész

--- fooldal.js
+++ fooldal.js
@@ -3,9 +3,10 @@ function QS(elem){
     return document.querySelector(elem)
 }
 
-const hero = QS('.hero')
-const slider = QS('.slider')
-const headline = QS('.headline')
+const hero = QS('.hero');
+const slider = QS('.slider');
+const headline = QS('.headline');
+const navbar = QS('.navbar');
 
 var tl = gsap.timeline();
 
@@ -13,3 +14,4 @@ tl.fromTo(hero,1,{height: "0%"},{ height:"80%", ease: Power2.easeInOut})
 .fromTo(hero, 1.2,{width: "100%"},{ width:"80%", ease: Power2.easeInOut})
 .fromTo(slider,1.2,{x:"-100%"},{x:"0%", ease: Power2.easeInOut},"-=1.2")
 .fromTo(headline,1.2,{x:"-100%"},{x:"10%", ease: Power2.easeInOut},"-=1.2")
+.fromTo(navbar,1.2,{x:"-100%"},{x:"0%", ease: Power2.easeInOut},"-=1.2")
