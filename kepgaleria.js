f3fade7acb4c5d259f0389638954eca39d5f8e34
Szazidayum <szabrinamolnar@gmail.com>
2022-04-17 13:00:50 +0200

    automatikus képbetöltés js-ből + proba json file

--- kepgaleria.js
+++ kepgaleria.js
@@ -2,9 +2,13 @@
 window.addEventListener("load",MSZ_init);
 
 let slideIndex = 0;
+
 function MSZ_init(){
     MSZ_slider();
+
+    jsonbolOlvas();
 }
+
 
 
 function MSZ_slider() {
@@ -22,4 +26,50 @@ function MSZ_slider() {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   setTimeout(MSZ_slider, 2000);
+}
+
+/*function MSZ_kepbetolt(){
+  let txt=`<div class="MSZ_content">
+  <h2 class="MSZ_title">Címe a ruhának</h2>
+  <p class="MSZ_copy">
+    Szerő: XY<br />
+    Fotós neve: ZX
+  </p>
+  <a href="kepgaleria.html"
+    ><button class="MSZ_btn">Tovább az oldalra</button></a
+  >
+</div>`;
+  document.getElementById("MSZ_kartya").innerHTML=txt;
+  console.log(getElementById());
+}*/
+
+function MSZ_kepbetolt(data){
+console.log(data);
+
+  if ('content' in document.createElement('template')){
+    var tDIV = document.querySelector("#MSZ_tarolo");
+    var galeriaDIV = document.querySelector('#MSZ_galeria');
+    
+    
+    data.kepek.forEach(element => {
+      var clone = tDIV.content.cloneNode(true);
+    var td = clone.querySelectorAll("span");
+    td[0].textContent = element.cim;
+    td[1].textContent = element.szerzo;
+    td[2].textContent = element.fotosnev;
+    clone.querySelector(".MSZ_kartya").style.backgroundImage = `url(${element.eleresiut})`;
+    clone.querySelector("a").href = element.tovabboldalra;
+    galeriaDIV.appendChild(clone);
+    });
+
+  }else{
+
+  }
+  console.log();
+}
+
+function jsonbolOlvas(){
+  fetch('kepek.json')
+  .then(response => response.json())
+  .then(data => MSZ_kepbetolt(data));
 }
