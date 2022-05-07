ba4c1a128ebee67876971627f92bcaf2e1d06c3e
KncskBenedek <koncsikbenedek@gmail.com>
2022-05-07 13:14:11 +0200

    kezdetleges behelyettesítés

--- bebe/scriptek/ruhaBemutato.js
+++ bebe/scriptek/ruhaBemutato.js
@@ -6,6 +6,18 @@ localStorage.getItem("aktualis");
     jobra gomb
     kisképre kattintva nagyképként kiemel
 */
-window.addEventListener("load", function (){
-        
+function ID(elem) {
+    return document.getElementById(elem);
+}
+window.addEventListener("load", function () {
+    /* const elem = localStorage.getItem("aktualis");
+    console.log(elem);
+    fetch()
+    .then(response => response.json())
+    .then(data => function (data){
+        console.log(data); */
+    var cucc = JSON.parse(localStorage.getItem("aktualis"));
+    console.log(cucc);
+    ID("fotos").innerHTML = cucc.fotosnev;
+
 });
