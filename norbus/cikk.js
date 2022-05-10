d0900f00db806c6f037897097c378df3f2ebda60
Norbus2000 <odornorbert.on@gmail.com>
2022-05-10 10:56:16 +0200

    json kesz kepek mappaszerkezet a cikkekhez

--- norbus/cikk.js
+++ norbus/cikk.js
@@ -4,7 +4,7 @@ function $(elem) {
 }
 
 function tartalom(){
-  fetch("forras.json")
+  fetch("ujForras.json")
     .then((response) => response.json())
     .then((data) => {
       console.log(data.cikkek);
@@ -15,28 +15,49 @@ function tartalom(){
 
 
 }
+
+function benneVan(cikkJson, cikkNeve) {
+  return Object.prototype.hasOwnProperty.call(cikkJson,cikkNeve)
+}
+
+
+
 function megjelenit(cikkek) {
 
-  txt ="";
-  for (let index = 0; index < cikkek.length; index++) {
-    txt += `<h1>${cikkek[index].cim}</h1>`;
-    for (const kulcs in cikkek[index].alcimek) {
-      txt += `<h1>${kulcs}</h1>`;
+  var bekezdSzam = 1;
+  var alcimSzam = 1;
+
+  for (const kulcs in cikkek) {
+    txt = "";
+    console.log(cikkek[kulcs].cim);
+    if(benneVan(cikkek[kulcs], "cim")) {
+      txt += `<h1>${cikkek[kulcs].cim}</h1>`;
     }
-    if ("felsorolasok" in cikkek[index]) {
-      txt += "<br>";
-      for (const ertek in cikkek[index].felsorolasok) {
-        txt += `<h1>${ertek}</h1>`;
-      }
-    } else {
-      console.log("nincs");
+    if(benneVan(cikkek[kulcs], ("bekezd" + bekezdSzam))) {
+      console.log(("bekezd" + bekezdSzam));
+      bekezdes = ("bekezd" + bekezdSzam);
+      txt += `<h3>${cikkek[kulcs][bekezdes]}</h3>`;
+      bekezdSzam++;
+    }
+    if(benneVan(cikkek[kulcs], ("alcim" + alcimSzam))) {
+      aclim = ("alcim" + alcimSzam);
+      txt += `<p>${cikkek[kulcs][alcim]}</p>`;
+      alcimSzam++;
     }
     
+    if(benneVan(cikkek[kulcs], "kep")) {
+      //txt += `<img${cikkek[kulcs].kep}`;
+    }
+    if(benneVan(cikkek[kulcs], "link")) {
+      txt += `<a href="${cikkek[kulcs].link}">Forrás</a>`;
+    } 
+    if(benneVan(cikkek[kulcs], "szerzo")) {
+      txt += `<p>${cikkek[kulcs].szerzo}</p>`;
+    }
+    }
+    $(".cikkek").innerHTML = txt;
   }
-  console.log(txt)
-  $(".cikkek").innerHTML += txt;
-}
-  
+    
   /*for (const cikkKulcs in cikkek[index]) {
       }
       
