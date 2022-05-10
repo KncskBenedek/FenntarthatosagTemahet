8295e45fd919bccd9bf53d238f1eb3c4d019f4e7
KncskBenedek <koncsikbenedek@gmail.com>
2022-05-10 11:54:36 +0200

    nagykép nem ugrál

--- bebe/scriptek/ruhaBemutato.js
+++ bebe/scriptek/ruhaBemutato.js
@@ -36,13 +36,13 @@ window.addEventListener("load", function () {
     
     let hossz = ruhaJson.kepek.length;
     for (let index = 0; index < hossz; index++) {
-        //kepekArr.push(ruhaJson.kepek[index]);
+        kepekArr.push(ruhaJson.kepek[index]);
         if (index === 0) {
             indicators += `<li data-target="#nagyKep" data-slide-to="${index}" class="active"></li>`;
             kepek += `<div class="carousel-item active"><img src="${ruhaJson.kepek[index]}" alt="Los Angeles" /></div>`;
         } else {
             indicators += `<li data-target="#nagyKep" data-slide-to="${index}"></li>`;
-            kepek += `<div class="carousel-item"><img src="${ruhaJson.kepek[index]}" alt="Los Angeles" /></div>`;
+            kepek += `<div class="carousel-item"><img src="${ruhaJson.kepek[index]}" class="belsoNagyKep" alt="Los Angeles" /></div>`;
         }
 
     }
@@ -60,7 +60,27 @@ window.addEventListener("load", function () {
     console.log(gombok);
     let nagykep = indicators + kepek + gombok;
     ID("nagyKep").innerHTML += nagykep;
-
+    console.log(kepekArr);
+    kezd(kepekArr);
 }
 });
 
+function kezd(tombKepek){
+    let alsoKepek = "";
+    if(tombKepek.length <=3){
+        alsoKepek = `<div id="kisLeptetoCont"> <div id="kisKepek" class="${tombKepek.length === 2?"ketKisKep":"alapKiskepek"}">`;
+    }else{
+        alsoKepek = `<div id="kisLeptetoCont"> <button id="bal"><<</button> <div id="kisKepek" class="alapKiskepek">`;
+    }
+    let meddig = alsoKepek.length === 2?2:3;
+    for (let index = 0; index < meddig; index++) {
+        alsoKepek+= `<div> <img src="${tombKepek[index]}"></div>`;
+    }    
+    if(tombKepek.length <=3){
+    alsoKepek += `</div></div> `;
+    
+    }else{
+        alsoKepek += `</div> <button id="jobb">>></button> </div> `;
+    }
+    ID("kepekContainer").innerHTML += alsoKepek;
+}
