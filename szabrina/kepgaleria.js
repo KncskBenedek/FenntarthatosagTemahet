afffee262261b92dd597a06387fc862853a346b1
Szazidayum <szabrinamolnar@gmail.com>
2022-05-15 11:43:29 +0200

    galéria

--- szabrina/kepgaleria.js
+++ szabrina/kepgaleria.js
@@ -1,31 +1,42 @@
 
 window.addEventListener("load",MSZ_init);
 
+function CLASS(nev){
+  return document.getElementsByClassName(nev);
+}
+
 let slideIndex = 0;
 
 function MSZ_init(){
     MSZ_slider();
+    MSZ_slider2();
     jsonbolOlvas();
   }
-
 
 
 function MSZ_slider() {
   let i;
   let slides = document.getElementsByClassName("MSZ_slide");
-  let dots = document.getElementsByClassName("MSZ_csik");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
   setTimeout(MSZ_slider, 2000);
 }
+function MSZ_slider2() {
+  let i;
+  let slides = document.getElementsByClassName("MSZ_slide2");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(MSZ_slider2, 2000);
+}
+
 
 function MSZ_kepbetolt(data) {
   console.log(data);
